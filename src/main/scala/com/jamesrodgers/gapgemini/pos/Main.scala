15566fae5fae505276f21743765354ff6cb2a3d6
James <james.rodgers@.com>
2017-08-23 14:53:30 +0100

    Wire up functions in Main
    Step 1 complete

--- src/main/scala/com/jamesrodgers/gapgemini/pos/Main.scala
+++ src/main/scala/com/jamesrodgers/gapgemini/pos/Main.scala
@@ -2,7 +2,10 @@ package com.jamesrodgers.gapgemini.pos
 
 
 object Main extends PointOfSale with App {
-  println(message)
+    val prompt = "(a: Apple, o: Orange, t: Total, c: Cancel) > "
+    val scanAndSum = readProducts _ andThen mkLineItems andThen receipt
+
+    scanAndSum(prompt).foreach(println)
 }
 
 
